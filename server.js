feff2d535ab2edfe638c4f411130b9d964c59d71
mohamed2002p <120063088+mohamed2002p@users.noreply.github.com>
2024-02-03 18:38:09 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const User = require("./src/route/userRoute");
 const cors = require('cors'); 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, '')));
+app.use(express.static(path.join(__dirname)));
 const port = 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,7 +17,7 @@ connectDB()
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'src/asset/home.html'));
 // });
-app.use("/.netlify/functions/api",User);
+app.use("/",User);
 module.exports.handler = serverless(app);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
